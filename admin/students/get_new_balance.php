f3725a8e50177ca741746046edec7e315a3f5ba3
mohamed <mr319242@gmail.com>
2023-09-05 09:34:08 +0300

    end student

--- admin/students/get_new_balance.php
+++ admin/students/get_new_balance.php
@@ -7,11 +7,9 @@ if (isset($_GET['student_id'])) {
     // Fetch the new balances for all students from the database
     $stmt = $connect->prepare("SELECT id, balance FROM students");
     $stmt->execute();
-
     $newBalances = array();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $newBalances[$row['id']] = $row['balance'];
     }
-
     echo json_encode($newBalances);
 }
